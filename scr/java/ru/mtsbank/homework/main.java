165688aab56fe8477eba56e52cd3e197830ff810
anvarishe <anvarishe@gmail.com>
2024-03-09 20:50:19 +0300

    branch

--- scr/java/ru/mtsbank/homework/main.java
+++ scr/java/ru/mtsbank/homework/main.java
@@ -5,5 +5,6 @@ public class main {
         System.out.println("Hello World!!! Hello hello");
         System.out.println("I'm here");
         System.out.println("branch");
+        System.out.println("242");
     }
 }
