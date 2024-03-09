f1d8888bc7dd13902543ef34382b67e29f049bc9
anvarishe <anvarishe@gmail.com>
2024-03-09 21:05:03 +0300

    312312

--- scr/java/ru/mtsbank/homework/main.java
+++ scr/java/ru/mtsbank/homework/main.java
@@ -3,10 +3,5 @@ package ru.mts.homework;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!!! Hello hello");
-        System.out.println("I'm here");
-        System.out.println("branch");
-        System.out.println("Hello World!!! Hello hello");
-        System.out.println("I'm here");
-        System.out.println("branch");
     }
 }
