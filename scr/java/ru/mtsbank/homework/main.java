172b7e327293beb17509a117ab80e9cca553d851
anvarishe <anvarishe@gmail.com>
2024-03-09 19:54:00 +0300

    EXAMPLE-TASK2

--- scr/java/ru/mtsbank/homework/main.java
+++ scr/java/ru/mtsbank/homework/main.java
@@ -4,6 +4,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!!! Hello hello");
         System.out.println("I'm here");
-        System.out.println("Stay tuned and chill!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println("Stay tuned and chill!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
 }
