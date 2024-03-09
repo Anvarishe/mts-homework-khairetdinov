f6e3db8a93cb817543d47a61f8950fcb10371e5e
anvarishe <anvarishe@gmail.com>
2024-03-09 19:04:31 +0300

    dev commit

--- scr/java/ru/mtsbank/homework/main.java
+++ scr/java/ru/mtsbank/homework/main.java
@@ -3,5 +3,6 @@ package ru.mts.homework;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!!! Hello hello");
+        System.out.println("I'm here");
     }
 }
